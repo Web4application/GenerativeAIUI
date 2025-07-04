a09e60835e44ebecf9af4624d9387ce883504d5d
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-04 05:29:46 +0200

    Update and rename Helm.sh to helm.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- helm.sh
+++ helm.sh
@@ -1,3 +1,8 @@
+$ helm3 plugin install https://github.com/helm/helm-2to3
+Downloading and installing helm-2to3 v0.2.0 ...
+https://github.com/helm/helm-2to3/releases/download/v0.2.0/helm-2to3_0.2.0_darwin_amd64.tar.gz
+Installed plugin: 2to3
+
 helm repo add gitlab https://charts.gitlab.io
 helm repo update
 helm upgrade --install web4 gitlab/gitlab-agent \
